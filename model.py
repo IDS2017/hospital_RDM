445925729c0a4759f9192f981ff225670e951c0f
Wellington <wellingtonhsu@gmail.com>
2017-04-20 02:19:02 -0400

    delete commenting

--- model.py
+++ model.py
@@ -8,34 +8,30 @@ from sklearn import linear_model, datasets
 from sklearn.datasets import make_hastie_10_2
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
-from matplotlib.lines import Line2D
+from matplotlib.lines import Line2
 
 
 
 def L1logisticReg(X,Y):
     score = {}
-    #Regularisation strength, the smaller the stronger regularisation
     cs = [10**i for i in range(-5,5)]
     for c in cs:
         score[c]  = []
         # Parameter c specifies regularisation strength, the smaller the stronger strength
         clf = linear_model.LogisticRegression(C = c, solver='liblinear')
         clf.fit(X, Y)
-        #clf.predict(Xtest)
         score[c].append(cross_val_score(clf, X, Y, scoring="accuracy", cv=5))
     return score
 
 
 def L2logisticReg(X,Y):
     score = {}
-    #Regularisation strength, the smaller the stronger regularisation
     cs = [10**i for i in range(-5,5)]
     for c in cs:
         score[c]  = []
         # Parameter c specifies regularisation strength, the smaller the stronger strength
         clf = linear_model.LogisticRegression(C = c, solver='lbfgs')
         clf.fit(X, Y)
-        #clf.predict(Xtest)
         score[c].append(cross_val_score(clf, X, Y, scoring="accuracy", cv=5))
     return score
 
@@ -48,7 +44,6 @@ def randomForest(X, Y):
         #n_estimators is The number of trees in the forest.
         clf = RandomForestClassifier(n_estimators=c)
         clf.fit(X, Y)
-        #clf.predict(Xtest)
         score[c].append(cross_val_score(clf, X, Y, scoring="accuracy", cv=5))
     return score
 
@@ -61,8 +56,6 @@ def linearSVM(X, Y):
         #Penalty parameter C of the error term
         clf = LinearSVC(C=c)
         clf.fit(X, Y)
-        #clf.predict(Xtest)
-        #score[c] = clf.score(Xtest, Ytest)
         score[c].append(cross_val_score(clf, X, Y, scoring="accuracy", cv=5))
     return score
 
@@ -76,8 +69,6 @@ def NuSVM(X, Y):
         #nu is an upper bound on the fraction of training errors and a lower bound of the fraction of support vectors.
         clf = NuSVC(nu=c, decision_function_shape='ovo')
         clf.fit(X, Y)
-        #clf.predict(Xtest)
-        #score[c] = clf.score(Xtest, Ytest)
         score[i].append(cross_val_score(clf, X, Y, scoring="accuracy", cv=5))
         i+=1
     return score
