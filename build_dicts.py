73e87c30fa98d454f32347e12ed231a752a7bc3a
Namssi <jn1664@nyu.edu>
2017-04-20 16:46:28 -0400

    change data structure

--- build_dicts.py
+++ build_dicts.py
@@ -3,34 +3,36 @@ import pandas as pd
 
 # 1. read the original data
 diabetic_data = pd.read_csv('data/diabetic_data.csv').drop('encounter_id', 1)
-meta = {'total_instances':diabetic_data.shape[0], 'categorical':{}, 'numerical':{}}
+meta = {'total_instances':diabetic_data.shape[0], 'used_cols':{}}
 
-# 2. find Numerical columns
+# 2. device Numerical and Categorical columns
 num_data = diabetic_data._get_numeric_data()
 for col_name in num_data:
     if 'id' in col_name:
         num_data = num_data.drop(col_name, 1)
+cate_data = diabetic_data.drop(num_data.columns, 1)
 
-# 3. form a dictionary for Categorical and Numerical columns with column names
-for col_name in diabetic_data.columns:
-    if col_name in num_data.columns:
-        meta['numerical'][col_name] = {}
-    else:
-        meta['categorical'][col_name] = {}
-
-# 4. get information from Categorical columns
-for col_name in meta['categorical']:
-    meta['categorical'][col_name] = dict(diabetic_data[col_name].describe())
-    meta['categorical'][col_name]['categories'] = []
-    meta['categorical'][col_name]['cate_cnt'] = {}
+# 3. get information from Categorical columns
+for col_name in cate_data.columns:
+    meta['used_cols'][col_name] = dict(diabetic_data[col_name].describe())
+    meta['used_cols'][col_name]['categories'] = []
+    meta['used_cols'][col_name]['cate_cnt'] = {}
     for unique_val in diabetic_data[col_name].unique():
         if 'diag_' not in col_name:
-            meta['categorical'][col_name]['categories'].append(unique_val)
-            meta['categorical'][col_name]['cate_cnt'][unique_val] = diabetic_data[col_name].value_counts()[unique_val]
+            meta['used_cols'][col_name]['categories'].append(unique_val)
+            meta['used_cols'][col_name]['cate_cnt'][unique_val] = diabetic_data[col_name].value_counts()[unique_val]
 
-# 5. get information from Numerical columns
-for col_name in meta['numerical']:
-    meta['numerical'][col_name] = dict(diabetic_data[col_name].describe())
+# 4. get information from Numerical columns
+for col_name in num_data.columns:
+    meta['used_cols'][col_name] = dict(diabetic_data[col_name].describe())
+
+# 5. set information whether the column value is numeric or categorical
+for col_name in diabetic_data.columns:
+    if col_name in num_data.columns:
+        meta['used_cols'][col_name]['data_type'] = 'numeric'
+    else:
+        meta['used_cols'][col_name]['data_type'] = 'categorical'
+
 
 print(meta)
     #print(col_name, ':', unique)
